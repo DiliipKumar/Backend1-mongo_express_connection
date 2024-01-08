94a4f25f70f49209d55f970b4a423d53da102481
diliip <dilip@DilipKumar>
2024-01-08 22:12:00 +0530

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config({
+    path: '../.env'
+})  
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const mongoURI = process.env.MONGODB_URI;
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${mongoURI}/${DB_NAME}`);
+
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
