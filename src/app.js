149a4b933c44664a5cc5ffb428a9c49269ef998a
diliip <dilip@DilipKumar>
2024-01-11 15:40:02 +0530

    your message

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
-import express from "express"
-const app = express()
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+const app=express();
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 
 
 
-export { app }
+export default app;
