f04fd600b0f004a70b078b5c4d122ae9afcd6799
diliip <dilip@DilipKumar>
2024-01-13 13:41:55 +0530

    your message

--- src/app.js
+++ src/app.js
@@ -5,13 +5,19 @@ import cors from 'cors'
 const app=express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials :true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//importing route
+import userRouter from './routes/user.routes.js'
 
-
+//Declearning route
+app.use("/api/v1/users",userRouter)
 
 
 export default app;
